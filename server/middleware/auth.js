d6060315c5e843319b2ca2c8fde226585c55f934
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-10-16 15:33:25 -0700

    refactoring minor code

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,29 +6,27 @@ module.exports.createSession = (req, res, next) => {
   req.session = {};
   if (req.cookies.shortlyid === undefined) {
     models.Sessions.create()
-      .then((results) => {
+      .then( (results) => {
         return models.Sessions.get({ id: results.insertId });
       })
-      .then((results) => {
+      .then( (results) => {
         req.session = results;
-        //req.session.user = 'Tester';
         res.cookie('shortlyid', results.hash);
         next();
       });
   } else {
-    models.Sessions.get({hash: req.cookies.shortlyid})
+    models.Sessions.get({ hash: req.cookies.shortlyid })
       .then( (results) => {
         req.session = results;
-
         res.cookie('shortlyid', results.hash);
         next();
       })
       .catch( () => {
         models.Sessions.create()
-          .then((results) => {
+          .then( (results) => {
             return models.Sessions.get({ id: results.insertId });
           })
-          .then((results) => {
+          .then( (results) => {
             req.session = results;
             res.cookie('shortlyid', results.hash);
             next();
